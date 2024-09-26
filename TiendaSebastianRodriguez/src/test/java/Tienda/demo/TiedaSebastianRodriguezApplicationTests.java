907b6e118eb6f8f97a40e5bccfa0559f2a9cd56d
Sebas090909 <tanyr09@gmail.com>
2024-09-25 20:05:29 -0600

    Creacion de Proyecto
    Se creo proyecto desde 0

---
+++ TiendaSebastianRodriguez/src/test/java/Tienda/demo/TiedaSebastianRodriguezApplicationTests.java
@@ -0,0 +1,13 @@
+package Tienda.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiedaSebastianRodriguezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
