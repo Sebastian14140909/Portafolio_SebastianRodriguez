35fa2c482d310130709a970b267d7a9f7f331d46
Sebas090909 <tanyr09@gmail.com>
2024-10-04 16:38:48 -0600

    Semana 2
    Este es el push orginal de semana 2,
    se solucionaron errores y se muestra correctamente el css

--- TiendaSebastianRodriguez/src/main/java/Tienda/demo/TiedaSebastianRodriguezApplication.java
+++ TiendaSebastianRodriguez/src/main/java/Tienda/demo/TiedaSebastianRodriguezApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TiedaSebastianRodriguezApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TiedaSebastianRodriguezApplication.class, args);
+             SpringApplication.run(TiedaSebastianRodriguezApplication.class, args);
 	}
 
 }
