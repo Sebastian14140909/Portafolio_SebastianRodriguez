f64a6ca35405ba3b7c62c524de33c0b9b2700cf8
Sebas090909 <tanyr09@gmail.com>
2024-10-22 19:11:18 -0600

    Portafolio Semana 5
    Portafolio semana 5 todo actuaizado, lo unico no funcional por el momento seria el boton de categorias

---
+++ TiendaSebastianRodriguez/src/main/java/com/tienda/controller/CategoriaController.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda.controller;
+
+import com.tienda.service.CategoriaService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("/categoria")
+public class CategoriaController {
+    
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @GetMapping("/listado")
+    public String inicio(Model model) {
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("categorias", categorias);
+        model.addAttribute("totalCategorias", categorias.size());
+        return "/categoria/listado";
+    }
+}
